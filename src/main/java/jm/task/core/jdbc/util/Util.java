3cc6d02e2319a4367492ddf3ec11a2b9a708ddd3
Igor Ilkov <garikb@inbox.ru>
2024-10-04 19:11:29 +0500

    Add Task Two ver.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
     private static final String url = "jdbc:mysql://localhost:3306/pptask2";
     private static final String user = "root";
     private static final String password = "123456";
@@ -23,7 +22,6 @@ public class Util {
                 throw new RuntimeException(e);
             }
         }
-
     }
 
     public static Statement getStatement() {
